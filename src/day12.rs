066a24e6a366292e4c8445031827c5b07be5ed6e
Jonathan Cornaz <jonathan.cornaz@gmail.com>
2022-11-24 22:39:05 +0100

    remove result type from template

--- src/day12.rs
+++ src/day12.rs
@@ -1,17 +1,15 @@
 type Output = u64;
 
-pub fn part_1(input: &str) -> Result<Output, String> {
-    input.parse::<Output>().map_err(|e| e.to_string())
+pub fn part_1(input: &str) -> Output {
+    input.parse().unwrap()
 }
 
-pub fn part_2(input: &str) -> Result<Output, String> {
-    input.parse::<Output>().map_err(|e| e.to_string())
+pub fn part_2(input: &str) -> Output {
+    input.parse().unwrap()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day01::part_1;
-
     use super::*;
 
     const EXAMPLE: &str = r#"
@@ -26,7 +24,7 @@ mod tests {
     #[ignore = "not implemented"]
     #[case::input(INPUT, 0)]
     fn test_part_1(#[case] input: &str, #[case] expected: Output) {
-        assert_eq!(part_1(input.trim()).unwrap(), expected);
+        assert_eq!(part_1(input.trim()), expected);
     }
 
     #[rstest]
@@ -35,6 +33,6 @@ mod tests {
     #[ignore = "not implemented"]
     #[case::input(INPUT, 0)]
     fn test_part_2(#[case] input: &str, #[case] expected: Output) {
-        assert_eq!(part_2(input.trim()).unwrap(), expected);
+        assert_eq!(part_2(input.trim()), expected);
     }
 }
